02eeb64f3bd264c4616cb34a42ab9e6c6a4c6861
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2019-04-08 19:25:56 +0600

    footer.php file updated

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 <div class="well">
-			<h3>Website: /www.zamanitdevelopmentltd.com/
-				<span class="pull-right">Like Us: www.facebook.com/proDelowar</span>
+			<h3>Website: <a href="https://zamanwebdeveloper.com" target="_blank">https://zamanwebdeveloper.com</a>
+				<span class="pull-right">Like Us: <a href="https://www.facebook.com/ZamanWebDeveloper/" target="_blank">www.facebook.com/ZamanWebDeveloper/</a></span>
 			</h3>
 		</div>
 	</div>
